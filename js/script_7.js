c335c423b46138e318a95f00eaa4146691518160
ulia2007 <4iksssa@i.ua>
2021-12-07 09:00:08 +0200

    first commit

---
+++ js/script_7.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let number = 4.6;
+console.log(-4/0);
+console.log('string'*9);
+
+const persone = "Alex adasd ad ad asd ad asdasd ";
+
+const  bool = true;
+
+console.log(something);
+
+
+let und;
+console.log(und);
+
+const obj = {
+    name: 'john',
+    age: 25,
+    isMarried: false
+};
+ //console.log(obj.age);
+ console.log(obj['name']);
+
+ let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 6, {},[]];
+ console.log(arr[1]);
---
+++ js/script_8.js
@@ -0,0 +1,9 @@
+"use sctrict";
+
+const category = 'toys';
+
+console.log(`https://some.com/${category}/5`);
+
+const user = 'rybi';
+alert (`privet, ${user}`);  /*бэктики(`) исполнят роль конкатынации
+ после знак долара и фигурные скобки*/
